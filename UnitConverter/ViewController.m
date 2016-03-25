400844d8f675d9260d3d24545bc77cb0e6a0a424
Claudia Toh Yi An <Claudia@claudias-macbook-pro-2.local>
2016-03-25 16:05:45 +0800

    Complete working program.

--- UnitConverter/ViewController.m
+++ UnitConverter/ViewController.m
@@ -8,6 +8,25 @@
 
 #import "ViewController.h"
 
+double ConvertUnitOneToUnitTwo(double UnitOneValue){
+    double UnitTwoValue;
+    UnitTwoValue = UnitOneValue/(1/0.393701);
+    return UnitTwoValue;
+    
+}//Converts cm to Inches
+double ConvertUnitOneToUnitThree(double UnitOneValue){
+    double UnitThreeValue;
+    UnitThreeValue = UnitOneValue/(1/0.0328084);
+    return UnitThreeValue;
+    
+}//Converts cm to Foot
+double ConvertUnitOneToUnitFour(double UnitOneValue){
+    double UnitFourValue;
+    UnitFourValue = UnitOneValue/(1/0.01);
+    return UnitFourValue;
+    
+}//Converts cm to Meters
+
 @interface ViewController ()
 
 @property (weak, nonatomic) IBOutlet UITextField *inputField;
@@ -20,7 +39,22 @@
 - (IBAction)updateButton:(id)sender {
     NSMutableString *buf = [NSMutableString new];
     //stores a new string.
-    [buf appendString: @"Clicked!"];
+    double userInput = [self.inputField.text doubleValue];
+    
+    
+    if (self.segmentController.selectedSegmentIndex == 0){
+        double UnitTwoValue = ConvertUnitOneToUnitTwo(userInput);
+        [buf appendString: [@(UnitTwoValue) stringValue]];
+    }
+    else if (self.segmentController.selectedSegmentIndex == 1){
+        double UnitThreeValue = ConvertUnitOneToUnitThree(userInput);
+        [buf appendString: [@(UnitThreeValue) stringValue]];
+    }
+    else{
+        double UnitFourValue = ConvertUnitOneToUnitFour(userInput);
+        [buf appendString: [@(UnitFourValue) stringValue]];
+    }
+        
     self.outputField.text = buf;
     
 }
